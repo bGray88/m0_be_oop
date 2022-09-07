d37753fc4c4c82441810316768b9fd3c0893cb43
Brandon Gray <brandonjoe88@hotmail.com>
2022-09-06 21:19:46 -0600

    Add personal answers

--- oop.rb
+++ oop.rb
@@ -3,7 +3,20 @@
 # it should have a color attribute, that is silver by default
 # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
+class Unicorn
+    def initialize(hungry)
+        @hungry = hungry
+        @color = "silver"
+    end
 
+    def say(message)
+        return "*~*" + message + "*~*"
+    end
+end
+
+pegasus = Unicorn.new("no")
+p pegasus
+p pegasus.say("Come frolic with me!")
 
 #  Write a class called Vampire
 #  it should have a dynamic name attribute
@@ -11,6 +24,27 @@
 #  it should have a thirsty attribute, that is true by default
 #  it should have a drink method. When called, the thirsty attribute changes to false
 
+class Vampire
+    def initialize(name, pet = "bat")
+        @name = name
+        @pet = pet
+        @thirsty = true
+    end
+
+    def drink
+        @thirsty = false
+    end
+
+    def thirsty?
+        @thirsty
+    end
+end
+
+vlad = Vampire.new("Vlad", "Snake")
+p vlad
+p vlad.thirsty?
+vlad.drink
+p vlad.thirsty?
 
 #  Write a Dragon class
 #  it should have a dynamic name attribute (string)
@@ -19,6 +53,32 @@
 #  it should have a is_hungry attribute that is true by default
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
 
+class Dragon
+    def initialize(name, rider, color)
+        @name = name
+        @rider = rider
+        @color = color
+        @is_hungry = true
+        @times_eaten = 0
+    end
+
+    def eat
+        @times_eaten += 1
+        @times_eaten >= 4 ? @is_hungry = false : nil
+    end
+
+    def hungry?
+        @is_hungry
+    end
+end
+
+godzilla = Dragon.new("Godzilla", "Cransten", "Green")
+p godzilla
+p godzilla.hungry?
+3.times {godzilla.eat}
+p godzilla.hungry?
+godzilla.eat
+p godzilla.hungry?
 
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
@@ -28,3 +88,52 @@
 #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
 #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit
+    attr_reader :disposition
+
+    def initialize(name, disposition)
+        @name = name
+        @disposition = disposition
+        @age = 0
+        @is_adult = false
+        @is_old = false
+        @has_ring = false
+    end
+
+    def has_ring
+        @name == "Frodo" ? @has_ring = true : nil
+        @has_ring
+    end
+
+    def celebrate_birthday
+        @age += 1
+        @age >= 33 ? @is_adult = true : nil
+        @age >= 101 ? @is_old = true : nil
+    end
+
+    def adult?
+        @is_adult
+    end
+
+    def old?
+        @is_old
+    end
+end
+
+sam_wise = Hobbit.new("Sam", "Friendly")
+p sam_wise
+p sam_wise.disposition
+30.times {sam_wise.celebrate_birthday}
+p sam_wise.adult?
+p sam_wise.old?
+3.times {sam_wise.celebrate_birthday}
+p sam_wise.adult?
+65.times {sam_wise.celebrate_birthday}
+p sam_wise.old?
+3.times {sam_wise.celebrate_birthday}
+p sam_wise.old?
+p sam_wise.has_ring
+frodo_baggins = Hobbit.new("Frodo", "Adventurous")
+p frodo_baggins.disposition
+p frodo_baggins.has_ring
--- self-eval.md
+++ self-eval.md
@@ -1,5 +1,12 @@
 # Self Evaluation
 
-- This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
-- How is your workflow going, in terms of taking breaks, staying focused, etc.?
-- What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
+- This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?</br>
+--**"Everything flowed really well. I liked some of the challenges as they were not only testing but humorous as well.</br>
+I felt comfortable completing these challenges with the knowledge that I've acquired."**
+- How is your workflow going, in terms of taking breaks, staying focused, etc.?</br>
+--**"I think that I have a good pattern of working and breaks, but more importantly knowing when to take breaks so I remain</br>
+focused."**
+- What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?</br>
+--**"I've noticed that I am more comfortable in a room with less light, so I'll be working on making that happen. Seating</br>
+has also been an issue so I'll be looking for a solution there as well. Being more comfortable should make tackling problems</br>
+more manageable."**
